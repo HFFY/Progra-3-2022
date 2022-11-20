c2b73bceba153670f9e85625c59401f7161e3beb
HugoFlores <hugorilahffy@gmail.com>
2022-11-20 19:32:41 -0400

    merge Test

--- app/src/main/java/com/example/progra32022/activity/MainActivity.kt
+++ app/src/main/java/com/example/progra32022/activity/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.progra32022.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.progra32022.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.text_title)
+        textView.text = "Hola Mundo"
     }
 }
